c77c6f42ee1e52908bfde193b9776d5d8656101b
Jeremy Kun <j2kun@users.noreply.github.com>
2023-12-29 20:57:18 -0800

    start with git grep

--- entrypoint.py
+++ entrypoint.py
@@ -7,10 +7,52 @@
 # if you use pyaction:4.0.0 or higher as your base docker image.
 
 import os
+import pathlib
+
+import git
+import github
+
+# Supported regex patterns
+
+# Issue number assuming current repository
+ISSUE_NUMBER_RE = r'TODO[(]#([0-9]+)[)]: ?(.*)'
+
+
+def main(gh_repo, local_repo):
+    affected_issues = []
+
+    result = local_repo.git.grep(ISSUE_NUMBER_RE)
+
+    print(result)
+
+    # issues = gh_repo.get_issues()
+    # if issues:
+    #     for issue in issues:
+    #         print('-'*25)
+    #         print(f'#{issue.number}: {issue.title}')
+    #         print(f'{issue.body}')
+    #         for comment in issue.get_comments():
+    #             print(comment)
+
+    return affected_issues
+
 
 if __name__ == "__main__":
-    affected_issue = "#123"
+    auth = github.Auth.Token(os.environ.get("GITHUB_TOKEN"))
+    gh = github.Github(auth=auth)
+    gh_repo = gh.get_repo(os.environ.get("GITHUB_REPOSITORY"))
+
+    assert gh_repo, "Could not find github repo, quitting"
+
+    local_repo = git.Repo(pathlib.Path(__file__).parent.resolve())
+    assert not local_repo.bare, "Found bare repo, quitting"
+    assert not local_repo.is_dirty(), "Found dirty repo, quitting"
+
+    affected_issues = main(gh_repo, local_repo)
+
+    gh.close()
 
     if "GITHUB_OUTPUT" in os.environ:
         with open(os.environ["GITHUB_OUTPUT"], "a") as f:
-            print("{0}={1}".format("affected-issues", affected_issue), file=f)
+            print("{0}={1}".format("affected-issues",
+                  ','.join(affected_issues)), file=f)
