6d8403926eb30f956e509abf13a7fbb30cbb9b29
Jeremy Kun <j2kun@users.noreply.github.com>
2023-12-31 12:06:17 -0800

    integration test set equality check

--- tests/integration.py
+++ tests/integration.py
@@ -2,7 +2,7 @@ import os
 
 
 def test_affected_issues():
-    affected_issues = [
+    affected_issues = set(
         int(x) for x in os.environ.get("AFFECTED_ISSUES", "").split(",")
-    ]
-    assert affected_issues == [2, 4]
+    )
+    assert affected_issues == set([2, 4])
