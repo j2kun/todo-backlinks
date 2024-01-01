53f866734785e45e634f91dccfb86b4913ae3c9c
Jeremy Kun <j2kun@users.noreply.github.com>
2023-12-31 22:14:04 -0800

    ping closed issues with outstanding TODOs

--- entrypoint.py
+++ entrypoint.py
@@ -14,6 +14,7 @@ BOT_SIGNATURE = (
 )
 GITHUB_SERVER_URL = os.environ.get("GITHUB_SERVER_URL", "https://github.com")
 DRY_RUN = os.environ.get("DRY_RUN", "false").lower()[0] == "t"
+WARN_SENTINEL = "[!NOTE]"
 
 # Supported regex patterns; these need to be in whatever flavor of regex is
 # supported by git grep.
@@ -52,9 +53,18 @@ def single_todo_comment(todo: Todo) -> str:
     return f"[{todo.filepath}:{todo.line}]({link_url}): {todo.message}"
 
 
-def make_comment(todos: list[Todo]) -> str:
+def make_comment(todos: list[Todo], warn_closed: bool = False) -> str:
+    prefix = (
+        (
+            "> [!NOTE]\n"
+            + "> Re-commenting because this issue was closed with unresolved TODOs.\n\n"
+        )
+        if warn_closed
+        else ""
+    )
     return (
-        f"This issue has {len(todos)} outstanding TODOs:\n\n"
+        prefix
+        + f"This issue has {len(todos)} outstanding TODOs:\n\n"
         + "\n".join(" - " + single_todo_comment(t) for t in todos)
         + "\n\n"
         + BOT_SIGNATURE
@@ -62,9 +72,11 @@ def make_comment(todos: list[Todo]) -> str:
 
 
 def comment_up_to_date(
-    comment: github.IssueComment.IssueComment, todos: list[Todo]
+    comment: github.IssueComment.IssueComment,
+    todos: list[Todo],
 ) -> bool:
-    return comment.body == make_comment(todos)
+    expected_comment = make_comment(todos, warn_closed=WARN_SENTINEL in comment.body)
+    return comment.body == expected_comment
 
 
 def try_find_comment(issue):
@@ -74,9 +86,7 @@ def try_find_comment(issue):
     return None
 
 
-def main(gh_repo, local_repo) -> dict[int, Update]:
-    affected_issues: dict[int, Update] = dict()
-
+def populate_todos_from_source(local_repo):
     try:
         results = local_repo.git.grep("-n", ISSUE_NUMBER_RE)
     except git.exc.GitCommandError as e:
@@ -104,18 +114,33 @@ def main(gh_repo, local_repo) -> dict[int, Update]:
                 )
             )
 
+    for issue_number in todos:
+        todos[issue_number].sort(key=lambda t: t.filepath + ":" + t.line)
+
+    return todos
+
+
+def get_issue_and_bot_comment(gh_repo, issue_number):
+    try:
+        issue = gh_repo.get_issue(issue_number)
+    except:
+        issue = None
+
+    if not issue:
+        print(f"Unable to find issue linked by TODO: {issue_number}")
+        return None, None
+
+    print(f"Checking #{issue.number}: {issue.title}")
+    comment = try_find_comment(issue)
+    return issue, comment
+
+
+def populate_updates_from_todos(gh_repo, todos, affected_issues):
+    """Insert Updates into affected_issues using discovered TODOs in the codebase."""
     for issue_number, todo_list in todos.items():
-        try:
-            issue = gh_repo.get_issue(issue_number)
-        except:
-            issue = None
-
+        issue, comment = get_issue_and_bot_comment(gh_repo, issue_number)
         if not issue:
-            print(f"Unable to find issue linked by TODO: {todo_list[0]}")
             continue
-        print(f"Checking #{issue.number}: {issue.title}")
-
-        comment = try_find_comment(issue)
         if not comment:
             print(f"Found #{issue.number} has no comment referring to this TODO")
             affected_issues[issue_number] = Update(
@@ -131,7 +156,9 @@ def main(gh_repo, local_repo) -> dict[int, Update]:
                 new_comment_body=make_comment(todo_list),
             )
 
-    # delete comments whose TODOs are gone
+
+def populate_updates_from_needed_deletions(gh_repo, todos, affected_issues):
+    """Insert Updates into affected_issues for issues that need their comment deleted."""
     for issue in gh_repo.get_issues(state="open"):
         if issue.number in affected_issues or issue.number in todos:
             continue
@@ -144,6 +171,40 @@ def main(gh_repo, local_repo) -> dict[int, Update]:
                     new_comment_body="",
                     delete_comment=True,
                 )
+
+
+def populate_updates_for_prematurely_closed_issues(gh_repo, todos, affected_issues):
+    """Insert Updates into affected_issues for issues that were closed before
+    their TODOs were removed."""
+    for issue_number, todo_list in todos.items():
+        if issue_number in affected_issues:
+            continue
+
+        issue, comment = get_issue_and_bot_comment(gh_repo, issue_number)
+        if not issue:
+            continue
+        if comment and issue.state == "closed" and WARN_SENTINEL not in comment.body:
+            print(f"Found closed #{issue.number} with one or more outstanding TODOs")
+            # We want to delete and re-create the comment to ensure
+            # notifications fire. Could consider automatically re-opening the
+            # issue, but the most likely scenario is the TODO is stale and it
+            # should be deleted without reopening the issue.
+            affected_issues[issue_number] = Update(
+                todos=todo_list,
+                comment=comment,
+                new_comment_body=make_comment(todo_list, warn_closed=True),
+                delete_comment=True,
+            )
+            continue
+
+
+def main(gh_repo, local_repo) -> dict[int, Update]:
+    affected_issues: dict[int, Update] = dict()
+
+    todos = populate_todos_from_source(local_repo)
+    populate_updates_from_todos(gh_repo, todos, affected_issues)
+    populate_updates_from_needed_deletions(gh_repo, todos, affected_issues)
+    populate_updates_for_prematurely_closed_issues(gh_repo, todos, affected_issues)
 
     return affected_issues
 
@@ -167,11 +228,18 @@ if __name__ == "__main__":
         if DRY_RUN:
             continue
 
-        if update.comment:
+        if update.delete_comment:
+            update.comment.delete()
+            if update.new_comment_body:
+                issue = gh_repo.get_issue(issue_number)
+                issue.create_comment(update.new_comment_body)
+        elif update.comment and update.new_comment_body:
             update.comment.edit(update.new_comment_body)
-        else:
+        elif update.new_comment_body:
             issue = gh_repo.get_issue(issue_number)
             issue.create_comment(update.new_comment_body)
+        else:
+            print(f"Found #{issue_number} with a non-actionable update: {update}")
 
     gh.close()
 
--- tests/test_action.py
+++ tests/test_action.py
@@ -33,6 +33,7 @@ class FakeIssue:
     number: int
     title: str
     comments: list[FakeComment]
+    state: str = "open"
 
     def get_comments(self):
         return self.comments
@@ -107,34 +108,36 @@ def test_issue_with_no_comment_multiple_todos():
     )
 
     local_repo = FakeLocalRepo()
-    local_repo.add_match("myfile.py:123: TODO(#1): fix it")
     local_repo.add_match("foo/bar.py:5: TODO(#1): just do it")
+    local_repo.add_match("myfile.py:123: TODO(#1): fix it")
 
     actual = main(gh_repo, local_repo)
     expected = {
         1: Update(
             todos=[
                 Todo(
-                    filepath="myfile.py",
-                    line="123",
-                    issue_number=1,
-                    message="fix it",
-                ),
-                Todo(
                     filepath="foo/bar.py",
                     line="5",
                     issue_number=1,
                     message="just do it",
                 ),
+                Todo(
+                    filepath="myfile.py",
+                    line="123",
+                    issue_number=1,
+                    message="fix it",
+                ),
             ],
             comment=None,
             new_comment_body=(
                 "This issue has 2 outstanding TODOs:\n\n"
+                " - [foo/bar.py:5]"
+                "(https://github.com/j2kun/todo-backlinks/blob/main/foo/bar.py#L5)"
+                ": just do it\n"
                 " - [myfile.py:123]"
                 "(https://github.com/j2kun/todo-backlinks/blob/main/myfile.py#L123)"
-                ": fix it\n" + " - [foo/bar.py:5]"
-                "(https://github.com/j2kun/todo-backlinks/blob/main/foo/bar.py#L5)"
-                ": just do it\n\n" + BOT_SIGNATURE
+                ": fix it\n"
+                "\n" + BOT_SIGNATURE
             ),
         ),
     }
@@ -253,3 +256,64 @@ def tests_nonexistent_issue():
 
     actual = main(gh_repo, local_repo)
     assert len(actual) == 0
+
+
+def test_closed_issue_delete_comment_and_recreate():
+    bot_comment = (
+        "This issue has 1 outstanding TODOs:\n\n"
+        " - [myfile.py:123]"
+        "(https://github.com/j2kun/todo-backlinks/blob/main/myfile.py#L123)"
+        ": fix it\n\n" + BOT_SIGNATURE
+    )
+    issue_comment = FakeComment(bot_comment)
+    gh_repo = FakeGitHubRepo()
+    gh_repo.add_issue(
+        FakeIssue(
+            number=1,
+            title="test issue",
+            state="closed",
+            comments=[
+                FakeComment("This is my test issue"),
+                issue_comment,
+            ],
+        )
+    )
+
+    local_repo = FakeLocalRepo()
+    local_repo.add_match("myfile.py:123: TODO(#1): fix it")
+
+    actual = main(gh_repo, local_repo)
+
+    assert len(actual) == 1
+    assert actual[1].delete_comment
+    assert "[!NOTE]" in actual[1].new_comment_body
+    assert "was closed with unresolved TODOs" in actual[1].new_comment_body
+
+def test_closed_issue_ignore_after_note_added():
+    bot_comment = (
+        "> [!NOTE]\n"
+        "> Re-commenting because this issue was closed with unresolved TODOs.\n\n"
+        "This issue has 1 outstanding TODOs:\n\n"
+        " - [myfile.py:123]"
+        "(https://github.com/j2kun/todo-backlinks/blob/main/myfile.py#L123)"
+        ": fix it\n\n" + BOT_SIGNATURE
+    )
+    issue_comment = FakeComment(bot_comment)
+    gh_repo = FakeGitHubRepo()
+    gh_repo.add_issue(
+        FakeIssue(
+            number=1,
+            title="test issue",
+            state="closed",
+            comments=[
+                FakeComment("This is my test issue"),
+                issue_comment,
+            ],
+        )
+    )
+
+    local_repo = FakeLocalRepo()
+    local_repo.add_match("myfile.py:123: TODO(#1): fix it")
+
+    actual = main(gh_repo, local_repo)
+    assert actual == dict()
