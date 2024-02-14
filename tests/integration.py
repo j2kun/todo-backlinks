c3745671fa215840545336571fc397ac2948d66a
Jeremy Kun <j2kun@users.noreply.github.com>
2024-02-13 16:04:01 -0800

    fix test

--- tests/integration.py
+++ tests/integration.py
@@ -9,4 +9,4 @@ def test_affected_issues():
     # Issue #4 is in the impacted list because it has a comment that should be deleted.
     # TODO(#5): Ping a closed issue
     # TODO(#8): Don't update a proper comment
-    assert affected_issues == set([2, 4, 5, 8])
+    assert affected_issues == set([2, 4, 5])
